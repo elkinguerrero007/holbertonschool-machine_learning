4325aa183dc9aa53c92c4ad895c3ad4cbb8e7faa
elkinguerrero007 <3439@holbertonschool.com>
2022-08-25 20:24:53 -0500

    macine_learning

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
-"""evaluates based on a existing model"""
-import tensorflow as tf
 
 
+""" contains evaluate funct"""
+
+import tensorflow.compat.v1 as tf
+ 
 def evaluate(X, Y, save_path):
-    """
-    returns prediction, accuracy and loss_val
-    """
-    new_path = save_path + ".meta"
-    saver = tf.train.import_meta_graph(new_path)
+    """ return the y prediction, accuracy and the cost"""
     with tf.Session() as sess:
-        saver.restore(sess, save_path)
-        x = tf.get_collection("x")[0]
-        y = tf.get_collection("y")[0]
-        y_pred = tf.get_collection("y_pred")
-        loss = tf.get_collection("loss")
-        accuracy = tf.get_collection("accuracy")
-        pred, a_val, l_val = sess.run([y_pred, accuracy, loss], {x: X, y: Y})
-        return (pred[0], a_val[0], l_val[0])
+        new_saver = tf.train.import_meta_graph('model.ckpt.meta')
+        new_saver.restore(
+            sess,
+            save_path
+        )
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        loss = tf.get_collection('loss')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        y_pred = tf.get_collection('y_pred')[0]
+        y_pred, loss_value, accu_value = sess.run(
+            (y_pred, loss, accuracy), feed_dict={x: X, y: Y})
+        return y_pred, accu_value, loss_value
