d283d60af9b5827cc3ca255098b5ff2a5c186a0b
elkinguerrero007 <3439@holbertonschool.com>
2022-08-25 20:12:53 -0500

    macine_learning

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
-"""Evaluation method for classification model"""
-
-
+"""
+Module that builds, trains and saves a neural network classifier
+"""
 import tensorflow as tf
 
 
 def evaluate(X, Y, save_path):
-    """Evaluation method for classification model"""
+    """
+    a function builds, trains and saves a neural network classifier
+    :param X: a numpy.ndarray containing the input data
+    :param Y: a numpy.ndarray containing the labels
+    :param save_path: is the location to load the model from
+    :return: the network’s prediction, accuracy, and loss, respectively
+    """
     with tf.Session() as sess:
-        saver = tf.train.import_meta_graph(save_path + '.meta')
+        saver = tf.train.import_meta_graph(save_path + ".meta")
         saver.restore(sess, save_path)
-        x = tf.get_collection('x')[0]
+
+        x = tf.get_collection("x")[0]
         y = tf.get_collection("y")[0]
         y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
         loss = tf.get_collection("loss")[0]
-        acc = tf.get_collection("accuracy")[0]
-        vars = sess.run([y_pred, acc, loss], feed_dict={x: X, y: Y})
-        return vars[0], vars[1], vars[2]
+
+        y_eval = sess.run(y_pred, feed_dict={x: X, y: Y})
+        accuracy_eval = sess.run(accuracy, feed_dict={x: X, y: Y})
+        loss_eval = sess.run(loss, feed_dict={x: X, y: Y})
+        return y_eval, accuracy_eval, loss_eval
