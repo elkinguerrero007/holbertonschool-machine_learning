024cce9837b47b9b111d36a4a2c36b4e4896efd3
elkinguerrero007 <3439@holbertonschool.com>
2022-08-25 20:22:25 -0500

    macine_learning

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python3
-"""
-Module that builds, trains and saves a neural network classifier
-"""
+"""evaluates based on a existing model"""
 import tensorflow as tf
 
 
 def evaluate(X, Y, save_path):
     """
-    a function builds, trains and saves a neural network classifier
+    returns prediction, accuracy and loss_val
     """
+    new_path = save_path + ".meta"
+    saver = tf.train.import_meta_graph(new_path)
     with tf.Session() as sess:
-        saver = tf.train.import_meta_graph(save_path + ".meta")
         saver.restore(sess, save_path)
-
         x = tf.get_collection("x")[0]
         y = tf.get_collection("y")[0]
-        y_pred = tf.get_collection("y_pred")[0]
-        accuracy = tf.get_collection("accuracy")[0]
-        loss = tf.get_collection("loss")[0]
-
-        y_eval = sess.run(y_pred, feed_dict={x: X, y: Y})
-        accuracy_eval = sess.run(accuracy, feed_dict={x: X, y: Y})
-        loss_eval = sess.run(loss, feed_dict={x: X, y: Y})
-        return y_eval, accuracy_eval, loss_eval
+        y_pred = tf.get_collection("y_pred")
+        loss = tf.get_collection("loss")
+        accuracy = tf.get_collection("accuracy")
+        pred, a_val, l_val = sess.run([y_pred, accuracy, loss], {x: X, y: Y})
+        return (pred[0], a_val[0], l_val[0])
