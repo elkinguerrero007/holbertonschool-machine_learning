972486ec019a1556760ea1a1e5d9ae9b29f301a4
elkinguerrero007 <3439@holbertonschool.com>
2022-08-25 20:30:24 -0500

    macine_learning

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python3
+"""Evaluates the output of a neural network"""
 
 
-""" contains evaluate funct"""
+import tensorflow as tf
 
-import tensorflow.compat.v1 as tf
- 
+
 def evaluate(X, Y, save_path):
-    """ return the y prediction, accuracy and the cost"""
-    with tf.Session() as sess:
-        new_saver = tf.train.import_meta_graph('model.ckpt.meta')
-        new_saver.restore(
-            sess,
-            save_path
-        )
-        x = tf.get_collection('x')[0]
-        y = tf.get_collection('y')[0]
-        loss = tf.get_collection('loss')[0]
-        accuracy = tf.get_collection('accuracy')[0]
-        y_pred = tf.get_collection('y_pred')[0]
-        y_pred, loss_value, accu_value = sess.run(
-            (y_pred, loss, accuracy), feed_dict={x: X, y: Y})
-        return y_pred, accu_value, loss_value
+    """Evaluates the output of a neural network:
+    Args:
+        X (numpy.ndarray): containing the input data to evaluate
+        Y (numpy.ndarray): containing the one-hot labels for X
+        save_path (str): the location to load the model from
+    Returns:
+        the network’s prediction, accuracy, and loss, respectively
+    """
+    session = tf.Session()
+    saver = tf.train.import_meta_graph("{}.meta".format(save_path))
+    saver.restore(session, save_path)
+
+    x = tf.get_collection("x")[0]
+    y = tf.get_collection("y")[0]
+    y_pred = tf.get_collection("y_pred")[0]
+    acc = tf.get_collection("accuracy")[0]
+    loss = tf.get_collection("loss")[0]
+
+    eval_y_pred = session.run(y_pred, feed_dict={x: X, y: Y})
+    eval_accuracy = session.run(accuracy, feed_dict={x: X, y: Y})
+    eval_loss = session.run(loss, feed_dict={x: X, y: Y})
+
+    return eval_y_pred, eval_accuracy, eval_losss
