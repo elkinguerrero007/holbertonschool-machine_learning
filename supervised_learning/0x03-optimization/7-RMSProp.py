9919c34f22b5362e2f92cfd70696d7de5a204a73
elkinguerrero007 <elkin.10041@gmail..com>
2022-09-04 18:05:42 -0500

    optimization

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-""" RMSProp """
+"""
+Function that updates a variable using the RMSProp optimization algorithm
+"""
 
 
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
-    """ RMSProp """
-    s = beta2 * s + (1 - beta2) * grad ** 2
-    var = var - alpha * (1 / (s ** 0.5 + epsilon)) * grad
-    return var, s
-Footer
+    """
+    Returns: the updated variable and the new moment, respectively
+    """
+    new_moment = beta2 * s + (1 - beta2) * grad ** 2
+    updated_var = var - alpha * grad / (new_moment ** (1 / 2) + epsilon)
+    return updated_var, new_moment
