c9fc7d985183bbf501cf99107d3c7ba741c43945
elkinguerrero007 <elkin.10041@gmail..com>
2022-09-04 18:06:53 -0500

    optimization

--- supervised_learning/0x03-optimization/5-momentum.py
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-""" Momentum """
+"""
+Function that updates a variable using the gradient
+descent with momentum optimization algorithm
+"""
 
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
-    """ Momentum """
-    v = beta1 * v + (1 - beta1) * grad
-    var = var - alpha * v
-    return var, 
+    """
+    Returns: the updated variable and the new moment, respectively
+    """
+    momentum = beta1 * v + (1 - beta1) * grad
+    updated_var = var - alpha * momentum
+    return updated_var, momentum
