ed2b6c15da50536d4c6828a16c3cbede9b509f3b
elkinguerrero007 <3439@holbertonschool.com>
2022-09-13 12:22:30 -0500

    keras

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -1,34 +1,32 @@
 #!/usr/bin/env python3
-"""
-Builds a neural network with the Keras library
-"""
+"""builds a neural network with the Keras library"""
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
-    a function that builds a NN with Keras
-    :param nx: the number of input features to the network
-    :param layers: a list containing the number of nodes in each layer of the
-    network
-    :param activations: a list containing the activation functions used for
-    each layer of the network
-    :param lambtha: the L2 regularization parameter
-    :param keep_prob: the probability that a node will be kept for dropout
-    :return: the keras model
+    Args:
+        nx is the number of input features to the network
+        layers is a list containing the number of nodes in each
+        layer of the network
+        activations is a list containing the activation functions
+        used for each layer of the network
+        lambtha is the L2 regularization parameter
+        keep_prob is the probability that a node will be kept for dropout
+    Returns:
+        the keras model
     """
-    K.Model()
-    inputs = K.Input(shape=(nx,))
-    x = K.layers.Dense(layers[0], activation=activations[0],
-                       kernel_regularizer=K.regularizers.l2(lambtha))(inputs)
-    y = x
-    rate = 1 - keep_prob
-    for i in range(1, len(layers)):
-        if i == 1:
-            y = K.layers.Dropout(rate)(x)
+    regularizer = K.regularizers.L2(l2=lambtha)
+    for i in range(len(layers)):
+        if i == 0:
+            inputs = K.Input(shape=(nx, ))
+            pred = K.layers.Dense(units=layers[i],
+                                  activation=activations[i],
+                                  kernel_regularizer=regularizer)(inputs)
         else:
-            y = K.layers.Dropout(rate)(y)
-        y = K.layers.Dense(layers[i], activation=activations[i],
-                           kernel_regularizer=K.regularizers.l2(lambtha))(y)
-    model = K.Model(inputs=inputs, outputs=y)
-    return model
+            pred = K.layers.Dense(units=layers[i],
+                                  activation=activations[i],
+                                  kernel_regularizer=regularizer)(pred)
+        if i < len(layers) - 1:
+            pred = K.layers.Dropout(rate=(1 - keep_prob))(pred)
+    return K.Model(inputs=inputs, outputs=pred)
