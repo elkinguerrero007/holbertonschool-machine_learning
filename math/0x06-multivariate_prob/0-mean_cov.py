ee578b1df3d7937f6f8f838b13f5bb0fbb27cf36
elkinguerrero007 <3439@holbertonschool.com>
2022-11-07 20:41:59 -0500

    multivariate

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -1,37 +1,34 @@
 #!/usr/bin/env python3
 """
-Calculates the mean and covariance of a data set
+   Module containing function for
+   computing mean and covariance.
 """
+
+
 import numpy as np
 
 
 def mean_cov(X):
     """
-    a function that calculates the mean and covariance
-    :param X: numpy.ndarray of shape (n, d) containing the data set:
-        n is the number of data points
-        d is the number of dimensions in each data point
-        If X is not a 2D numpy.ndarray, raise a TypeError with the message X
-        must be a 2D numpy.ndarray
-        If n is less than 2, raise a ValueError with the message X must contain
-        multiple data points
-    :return: mean, cov:
-        mean is a numpy.ndarray of shape (1, d) containing the mean of the data
-        set
-        cov is a numpy.ndarray of shape (d, d) containing the covariance matrix
-        of the data set
+       Calculates the mean and covariance of
+        a dataset.
+       Args:
+        X: numpy.ndarray - shape (n, d) Containing data set.
+            n: Number of data points.
+            d: Number of dimensions in each data point.
+       Returns: mean, cov:
+        mean: numpy.ndarray - shape (1, d) Containing the
+            mean of the data set.
+        cov: numpy.ndarray - shape (d, d) Containing the
+            covariance matrix of the data set.
     """
-    if type(X) is not np.ndarray:
+    if type(X) is not np.ndarray or len(X.shape) != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
-    if len(X.shape) != 2:
-        raise TypeError("X must be a 2D numpy.ndarray")
-    n, d = X.shape
-    if n < 2:
+
+    if X.shape[0] < 2:
         raise ValueError("X must contain multiple data points")
 
-    mean = X.mean(axis=0).reshape(1, d)
-    X_mean = X - mean
-    cov = (X_mean.T @ X_mean) / (n - 1)
-    return mean, cov
-
-    
+    mean = X.sum(axis=0)/X.shape[0]
+    deviation = X - mean
+    covariant = np.matmul(deviation.T, deviation)
+    return mean[np.newaxis, ...], covariant/(X.shape[0]-1)
