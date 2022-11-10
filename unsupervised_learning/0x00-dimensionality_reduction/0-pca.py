6150d55c41f908c64d2041c46fbd1093afc471bd
elkinguerrero007 <3439@holbertonschool.com>
2022-11-09 19:24:51 -0500

    update

--- unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -1,27 +1,42 @@
 #!/usr/bin/env python3
 """
-Performs PCA on a dataset
+PCA on a dataset
 """
 import numpy as np
 
 
 def pca(X, var=0.95):
     """
-    a function that performs PCA on a dataset
-    :param X: numpy.ndarray of shape (n, d) where:
-        n is the number of data points
-        d is the number of dimensions in each point
-    :param var: the fraction of the variance that the PCA transformation
-    should maintain
-    :return: the weights matrix, W, that maintains var fraction of X‘s
-    original variance
+    Args:
+        X: is a numpy.ndarray of shape (n, d) where
+            n is the number of data points
+            d is the number of dimensions in each point
+            all dimensions have a mean of 0 across all data points
+        var is the fraction of the variance that the PCA transformation
+        should maintain
+    Returns:
+         the weights matrix, W, that maintains var fraction of X‘s
+         original variance
     """
-    u, s, vh = np.linalg.svd(X)
-    cum = np.cumsum(s)
-    thresh = cum[len(cum) - 1] * var
-    mask = np.where(thresh > cum)
-    var = cum[mask]
-    idx = len(var) + 1
-    W = vh.T
-    Wr = W[:, 0:idx]
-    return Wr
+    # SVD(Singular Value Deviation)
+    # U = Unitary array(s).
+    # vals = Vector(s) with the singular values,sorted in descending order.
+    # eig = Unitary array(s). eigen vectors
+    U, vals, eig = np.linalg.svd(X)
+    idx = np.argsort(vals)[::-1]
+    vals = vals[idx]
+    eig = (eig.T)[:, idx]
+
+    var_explained = []
+    eig_sum = vals.sum()
+
+    for i in range(vals.shape[0]):
+        var_explained.append(vals[i]/eig_sum)
+
+    # Cumulatice sum
+    Csum = np.cumsum(var_explained)
+
+    for i in range(Csum.shape[0]):
+        if Csum[i] >= var:
+            return eig[:, :i+1]
+    return eig
