dabd37d7ee68aebce1e22bd392ff963170033ded
elkinguerrero007 <3439@holbertonschool.com>
2022-11-17 16:53:14 -0500

    bic

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -1,77 +1,33 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Clustering
-"""
+"""Find best number of clusters using Bayesian Information Criterion."""
+
+
 import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
-    """
-    Function that finds the best number of clusters for a GMM using
-    the Bayesian Information Criterion
-    Arguments:
-     - X is a numpy.ndarray of shape (n, d) containing the data set
-     - kmin is a positive integer containing the minimum number of clusters
-        to check for (inclusive)
-     - kmax is a positive integer containing the maximum number of clusters
-        to check for (inclusive)
-     - iterations is a positive integer containing the maximum number of
-        iterations for the EM algorithm
-     - tol is a non-negative float containing the tolerance for
-        the EM algorithm
-     - verbose is a boolean that determines if the EM algorithm should print
-        information to the standard output
-    Returns:
-     best_k, best_result, l, b, or None, None, None, None on failure
-        - best_k is the best value for k based on its BIC
-        - best_result is tuple containing pi, m, S
-              * pi is a numpy.ndarray of shape (k,) containing the cluster
-                priors for the best number of clusters
-              * m is a numpy.ndarray of shape (k, d) containing the centroid
-                means for the best number of clusters
-              * S is a numpy.ndarray of shape (k, d, d) containing the
-                covariance matrices for the best number of clusters
-        - l is a numpy.ndarray of shape (kmax - kmin + 1) containing the
-            log likelihood for each cluster size tested
-        - b is a numpy.ndarray of shape (kmax - kmin + 1) containing the BIC
-            value for each cluster size tested
-            Use: BIC = p * ln(n) - 2 * l
-            * p is the number of parameters required for the model
-            * n is the number of data points used to create the model
-            * l is the log likelihood of the model
-    """
-
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    """Find best number of clusters using Bayesian Information Criterion."""
+    if ((type(X) is not np.ndarray or X.ndim != 2 or type(kmin) is not int
+         or kmin < 1 or type(verbose) is not bool or type(tol) is not float
+         or tol < 0 or type(iterations) is not int or iterations < 1)):
         return None, None, None, None
-    if type(kmin) != int or kmin <= 0 or kmin >= X.shape[0]:
+    if kmax is None:
+        kmax = X.shape[0]
+    if ((type(kmax) is not int or kmax <= kmin)):
         return None, None, None, None
-    if type(kmax) != int or kmax <= 0 or kmax >= X.shape[0]:
-        return None, None, None, None
-    if kmin >= kmax:
-        return None, None, None, None
-    if type(iterations) != int or iterations <= 0:
-        return None, None, None, None
-    if type(tol) != float or tol <= 0:
-        return None, None, None, None
-    if type(verbose) != bool:
-        return None, None, None, None
-
-    n, d = X.shape
-    k_r, result, l_b, b = [], [], [], []
-
+    best = None
+    bics = []
+    loglikes = []
     for k in range(kmin, kmax + 1):
-        pi, m, S, g, like = expectation_maximization(
-            X, k, iterations, tol, verbose)
-        k_r.append(k)
-        result.append((pi, m, S))
-        l_b.append(like)
-        p = (k * d * (d + 1) / 2) + (d * k) + k - 1
-        bic = p * np.log(n) - 2 * like
-        b.append(bic)
-    b = np.array(b)
-    best = np.argmin(b)
-    l_b = np.array(l_b)
-
-    return k_r[best], result[best], l_b[best], b[best]
+        pi, m, S, _, loglike = expectation_maximization(X, k,
+                                                        iterations=iterations,
+                                                        tol=tol,
+                                                        verbose=verbose)
+        bic = (6 * k - 1) * np.log(X.shape[0]) - 2 * loglike
+        if best is None or bics[best - kmin] > bic:
+            best = k
+            bestres = (pi, m, S)
+        bics.append(bic)
+        loglikes.append(loglike)
+    return best, bestres, np.asarray(loglikes), np.asarray(bics)
