a9ec2714909ab93e40584715b57bca731e0c61a9
elkinguerrero007 <3439@holbertonschool.com>
2022-11-17 17:28:44 -0500

    bic

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -1,53 +1,50 @@
 #!/usr/bin/env python3
+"""[summary]
+Returns:
+    [type]: [description]
 """
-Defines function that finds the best number of clusters for a GMM using
-the Bayesian Information Criterion (BIC)
-"""
-
-
 import numpy as np
-expectation_maximization = __import__('7-EM').expectation_maximization
+expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
+    """[summary]
+    Args:
+        X ([type]): [description]
+        kmin (int, optional): [description]. Defaults to 1.
+        kmax ([type], optional): [description]. Defaults to None.
+        iterations (int, optional): [description]. Defaults to 1000.
+        tol ([type], optional): [description]. Defaults to 1e-5.
+        verbose (bool, optional): [description]. Defaults to False.
+    Returns:
+        [type]: [description]
     """
-    Find the best number of clusters for a GMM using BIC
-    parameters:
-        X [numpy.ndarray of shape (n, d)]:
-            contains the dataset
-            n: the number of data points
-            d: the number of dimensions for each data point
-        kmin [positive int]:
-            the minimum number of clusters to check for (inclusive)
-        kmax [positive int]:
-            the maximum number of clusters to check for (inclusive)
-            if None, kmax should be set to maximum number of clusters possible
-        iterations [positive int]:
-            the maximum number of iterations for the algorithm
-        tol [non-negative float]:
-            the tolerance of the log likelihood, used for early stopping
-        verbose [boolean]:
-            determines if you should print information about the algorithm
-    should only use one loop
-    returns:
-        best_k, best_result, l, b
-            best_k [positive int]:
-                the best value for k based on its BIC
-            best_result [tuple containing pi, m, S]:
-                pi [numpy.ndarray of shape (k,)]:
-                    contains cluster priors for the best number of clusters
-                m [numpy.ndarray of shape (k, d)]:
-                    contains centroid means for the best number of clusters
-                S [numpy.ndarray of shape (k, d, d)]:
-                    contains covariance matrices for best number of clusters
-            l [numpy.ndarray of shape (kmax - kmin + 1)]:
-                contains the log likelihood for each cluster size tested
-            b [numpy.ndarray of shape (kmax - kmin + 1)]:
-                contains the BIC value for each cluster size tested
-                BIC = p * ln(n) - 2 * 1
-                    p: number of parameters required for the model
-                    n: number of data points used to create the model
-                    l: the log likelihood of the model
-        or None, None, None, None on failure
-    """
-    return None, None, None, None
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None, None
+    if type(kmin) != int or kmin <= 0 or kmin >= X.shape[0]:
+        return None, None, None, None
+    if type(kmax) != int or kmax <= 0 or kmax >= X.shape[0]:
+        return None, None, None, None
+    if kmin >= kmax:
+        return None, None, None, None
+    if type(iterations) != int or iterations <= 0:
+        return None, None, None, None
+    if type(tol) != float or tol <= 0:
+        return None, None, None, None
+    if type(verbose) != bool:
+        return None, None, None, None
+    n, d = X.shape
+    k_r, result, l_b, b = [], [], [], []
+    for k in range(kmin, kmax + 1):
+        pi, m, S, g, like = expectation_maximization(
+            X, k, iterations, tol, verbose)
+        k_r.append(k)
+        result.append((pi, m, S))
+        l_b.append(like)
+        p = (k * d * (d + 1) / 2) + (d * k) + k - 1
+        bic = p * np.log(n) - 2 * like
+        b.append(bic)
+    b = np.array(b)
+    best = np.argmin(b)
+    l_b = np.array(l_b)
+    return k_r[best], result[best], l_b[best], b[best]
