e55621c3bf787648cddcdb5638ce74d2cdab025f
elkinguerrero007 <3439@holbertonschool.com>
2022-11-17 18:02:10 -0500

    bic

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -1,50 +1,80 @@
 #!/usr/bin/env python3
 """
-module 9-BIC
-contains function BIC
+9 - BIC
 """
+
 import numpy as np
-expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """
-    Finds the best number of clusters for a GMM using
-    the Bayesian Information Criterion
+    Arguments:
+    - X: numpy.ndarray of shape (n, d) containing the data set
+    - kmin: positive integer containing the minimum number of
+            clusters to check for (inclusive)
+    - kmax: positive integer containing the maximum number of
+            clusters to check for (inclusive)
+    - If kmax is None, kmax should be set to the maximum number
+      of clusters possible
+    - iterations: positive integer containing the maximum number
+      of iterations for the EM algorithm
+    - tol: non-negative float containing the tolerance for the
+           EM algorithm
+    - verbose: boolean that determines if the EM algorithm should
+               print information to the standard output
+    Returns:
+    - best_k: best value for k based on its BIC
+    - best_result: tuple containing pi, m, S
+    - pi: numpy.ndarray of shape (k,) containing the cluster
+          priors for the best number of clusters
+    - m: numpy.ndarray of shape (k, d) containing the centroid
+         means for the best number of clusters
+    - S: numpy.ndarray of shape (k, d, d) containing the
+         covariance matrices for the best number of clusters
+    - l: numpy.ndarray of shape (kmax - kmin + 1) containing
+         the log likelihood for each cluster size tested
+    - b: numpy.ndarray of shape (kmax - kmin + 1) containing
+         the BIC value for each cluster size tested
     """
-    if type(X) is not np.ndarray or len(X.shape) is not 2:
-        return None, None, None, None
-    n, d = X.shape
-    if type(kmin) is not int or kmin <= 0 or kmin >= n:
-        return None, None, None, None
-    if type(kmax) is not int or kmax <= 0 or kmax >= n:
-        return None, None, None, None
-    if kmin >= kmax:
-        return None, None, None, None
-    if type(iterations) is not int or iterations < 1:
-        return None, None, None, None
-    if type(tol) is not float or tol <= 0:
-        return None, None, None, None
-    if type(verbose) is not bool:
-        return None, None, None, None
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None, None, None, None
 
-    b, results, ks, l_b = [], [], [], []
+    if type(kmin) != int or kmin <= 0:
+        return None, None, None, None, None, None
+
+    if kmax is None:
+        kmax = X.shape[0]
+
+    if type(kmax) != int or kmax <= 0:
+        return None, None, None, None, None, None
+
+    if type(iterations) != int or iterations <= 0:
+        return None, None, None, None, None, None
+
+    if type(tol) != float or tol < 0:
+        return None, None, None, None, None, None
+
+    if type(verbose) != bool:
+        return None, None, None, None, None, None
+
+    expectation_maximization = __import__('8-EM').expectation_maximization
+
+    l = []
+    b = []
 
     for k in range(kmin, kmax + 1):
-        ks.append(k)
-        pi, m, S, _, lklhd = expectation_maximization(X,
-                                                      k,
-                                                      iterations,
-                                                      tol,
-                                                      verbose)
-        results.append((pi, m, S))
-        l_b.append(lklhd)
-        p = (k * d * (d + 1) / 2) + (d * k) + k - 1
-        bic = p * np.log(n) - 2 * lklhd
+        pi, m, S, g, loglike = expectation_maximization(X, k, iterations,
+                                                        tol, verbose)
+        p = (k * m.shape[1]) + (k * m.shape[1] * (m.shape[1] + 1) / 2)
+        bic = p * np.log(X.shape[0]) - 2 * loglike
+        l.append(loglike)
         b.append(bic)
 
-    l_b = np.array(l_b)
-    b = np.array(b)
-    i = np.argmin(b)
+    l = np.asarray(l)
+    b = np.asarray(b)
+    best_k = np.argmin(b) + kmin
+    pi, m, S, g, loglike = expectation_maximization(X, best_k, iterations,
+                                                    tol, verbose)
+    best_result = (pi, m, S)
 
-    return ks[i], results[i], l_b, b
+    return best_k, best_result, l, b
