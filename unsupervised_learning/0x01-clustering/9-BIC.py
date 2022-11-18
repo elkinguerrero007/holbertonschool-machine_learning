fe7984528c5e96f2a2e4c922b491f8dc34b28c7e
elkinguerrero007 <3439@holbertonschool.com>
2022-11-17 19:07:23 -0500

    bic

--- unsupervised_learning/0x01-clustering/9-BIC.py
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -1,58 +1,38 @@
 #!/usr/bin/env python3
-"""
-9-BIC.py
-"""
+"""Contains the function BIC()"""
+
 import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
+    """Finds the best number of clusters for a GMM using the Bayesian
+    Information Criterion
+    Args:
+        X (ndarray)(n,d): data set
+        kmin (int): minimum no. clusters to check for
+        kmax (int): maximum no. clusters to check for
+        iterations (int): maximum no. iterations to use
+        tol (float): tolerance for EM algorithm
+        verbose (bool): determines if EM algorithm should print to stdout
+    Returns:
+        best_k, best_result, l, b // None, None, None, None if failure
+        best_k (int): best value for k
+        best_result (tuple)(pi, m, S):
+            pi (ndarray)(k,): cluster priors for best no. clusters
+            m (ndarray)(k,d): centroid means for best no. clusters
+            S (ndarray)(k,d,d): covariance matrices for best no. clusters
+        l (ndarray)(kmax-kmin+1): log likelihood for each cluster size tested
+        b (ndarray)(kmax-kmin+1): BIC value for each cluster size tested
     """
-    function that finds the best number of clusters for a GMM using
-    the Bayesian Information Criterion
-    """
-
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
+    if type(X) is not np.ndarray or X.ndim != 2:
         return None, None, None, None
-    if not isinstance(kmin, int) or kmin <= 0 or X.shape[0] <= kmin:
+    if kmax is None:
+        kmax = X.shape[0]
+    if (type(kmin) is not int or kmin <= 0 or kmin > X.shape[0] or
+       type(kmax) is not int or kmax <= 0 or kmax <= kmin or
+       kmax > X.shape[0] or
+       type(iterations) is not int or iterations <= 0 or
+       type(tol) is not float or tol < 0 or
+       type(verbose) is not bool):
         return None, None, None, None
-    if not isinstance(kmax, int) or kmax <= 0 or X.shape[0] <= kmax:
-        return None, None, None, None
-    if not isinstance(iterations, int) or iterations <= 0:
-        return None, None, None, None
-    if not isinstance(tol, float) or tol < 0:
-        return None, None, None, None
-    if not isinstance(verbose, bool):
-        return None, None, None, None
-
-    # X: array of shape (n, d) containing the data set
-    n, d = X.shape
-
-    # Define pi_t, m_t, S_t: arrays containing the relevant
-    # parameters for all the clusters
-    all_pis = []
-    all_ms = []
-    all_Ss = []
-    all_lkhds = []
-    all_bs = []
-
-    # Iterate over the ((kmax + 1) - kmin) clusters
-    for k in range(kmin, kmax + 1):
-        pi, m, S, g, lkhd = expectation_maximization(X, k, iterations,
-                                                     tol, verbose)
-        all_pis.append(pi)
-        all_ms.append(m)
-        all_Ss.append(S)
-        all_lkhds.append(lkhd)
-        # p: the number of parameters required for the model
-        p = (k * d * (d + 1) / 2) + (d * k) + (k - 1)
-        # b: array containing the BIC value for each cluster size tested
-        b = p * np.log(n) - 2 * lkhd
-        all_bs.append(b)
-
-    all_lkhds = np.array(all_lkhds)
-    all_bs = np.array(all_bs)
-    best_k = np.argmin(all_bs)
-    best_result = (all_pis[best_k], all_ms[best_k], all_Ss[best_k])
-
-    return best_k+1, best_result, all_lkhds, all_bs
