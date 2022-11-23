5c0de9d7bd06314e2a11cf6bd9df30828180fd19
elkinguerrero007 <3439@holbertonschool.com>
2022-11-23 12:09:28 -0500

    absorbing

--- unsupervised_learning/0x02-hmm/2-absorbing.py
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -1,39 +1,80 @@
 #!/usr/bin/env python3
 """
-Determines if a markov chain is absorbing
+Module contains function for determining
+if a markov chain is "absorbing".
 """
+
+
 import numpy as np
 
 
 def absorbing(P):
     """
-    Determines if a markov chain is absorbing
-    :param P: square 2D numpy.ndarray of shape (n, n) representing the
-    transition matrix
-    :return: True if it is absorbing, or False on failure
+    Determines if a markov chain is absorbing.
+    Args:
+        P: Square 2D numpy.ndarray - (n, n) Standard transition matrix.
+        P[i, j]: Probability of transitioning from state i to state j.
+        n: Number of states in the markov chain.
+    Return:
+      True if it is absorbing, or False on failure.
     """
-    if type(P) is not np.ndarray:
+
+    if type(P) is not np.ndarray or P.ndim != 2:
         return False
-    if len(P.shape) != 2:
+    if P.shape[0] != P.shape[1]:
         return False
-    n, n_t = P.shape
-    if n != n_t:
+
+    abs_state = 0
+    std = []
+
+    for x, row in enumerate(P):
+        if 1 in row:
+            std = [x] + std
+            abs_state += 1
+        else:
+            std.append(x)
+
+    if abs_state == 0:
         return False
-    sum_test = np.sum(P, axis=1)
-    for elem in sum_test:
-        if not np.isclose(elem, 1):
+    if abs_state >= P.shape[0] - 1:
+        return True
+
+    standard = (P[std, :])[:, std]
+
+    In = standard[:abs_state, :abs_state]
+    Q = standard[abs_state:, abs_state:]
+    F = np.linalg.inv(In-Q)
+
+    S = standard[abs_state:, :abs_state]
+
+    absorbtion_probs = np.matmul(F, S)
+    QF = np.matmul(F, Q)
+
+    lst = list(range(absorbtion_probs.shape[0]))
+    reached = []
+    pops = 0
+    for x, row in enumerate(absorbtion_probs):
+        test = row > 0
+        if True in test:
+            reached.append(x)
+            lst.pop(x-pops)
+            pops += 1
+
+    shp = 0
+    pops = 0
+
+    while True:
+        if shp != len(lst):
+            shp = len(lst)
+        else:
             return False
 
-    diagonal = np.diag(P)
-    if (diagonal == 1).all():
-        return True
+        for x in range(len(lst)):
+            test = Q[lst[x-pops], reached] > 0
+            if True in test:
+                reached.append(lst[x-pops])
+                lst.pop(x-pops)
+                pops += 1
 
-    absorb = (diagonal == 1)
-    for row in range(len(diagonal)):
-        for col in range(len(diagonal)):
-            if P[row, col] > 0 and absorb[col]:
-                absorb[row] = 1
-    if (absorb == 1).all():
-        return True
-
-    return False
+        if len(lst) == 0:
+            return True
