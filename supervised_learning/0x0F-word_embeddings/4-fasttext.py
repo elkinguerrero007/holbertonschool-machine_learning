b29abcd128a403e0ff1c8f32b34bc533a2bca8da
elkinguerrero007 <3439@holbertonstudents.com>
2023-01-10 11:31:13 -0500

    update

--- supervised_learning/0x0F-word_embeddings/4-fasttext.py
+++ supervised_learning/0x0F-word_embeddings/4-fasttext.py
@@ -1,40 +1,32 @@
 #!/usr/bin/env python3
 """
-File that contains the fasttext_model function
 """
-import gensim
+from gensim.test.utils import common_texts, get_tmpfile
+from gensim.models import FastText
 
 
 def fasttext_model(sentences, size=100, min_count=5, negative=5, window=5,
                    cbow=True, iterations=5, seed=0, workers=1):
     """
-    Function  that creates and trains a genism fastText model:
-    Arguments:
-        - sentences is a list of sentences to be trained on
-        - size is the dimensionality of the embedding layer
-        - min_count is the minimum number of occurrences of a word for use in
-                    training
-        - window is the maximum distance between the current and predicted word
-                 within a sentence
-        - negative is the size of negative sampling
-        - cbow is a boolean to determine the training type; True is for CBOW;
-               False is for Skip-gram
-        - iterations is the number of iterations to train over
-        - seed is the seed for the random number generator
-        - workers is the number of worker threads to train the model
-    Returns: 
-        - the trained model
+    :param sentences:
+    :param size:
+    :param min_count:
+    :param window:
+    :param negative:
+    :param cbow: boolean to determine the training type; True is for CBOW;
+    False is for Skip-gram
+    :param iterations:
+    :param seed:
+    :param workers:
+    :return:
     """
-    model = gensim.models.FastText(sentences=sentences,
-                                   size=size,
-                                   min_count=min_count,
-                                   window=window,
-                                   negative=negative,
-                                   sg=cbow,
-                                   iter=iterations,
-                                   seed=seed,
-                                   workers=workers)
-    model.train(sentences=sentences,
-                total_examples=model.corpus_count,
-                epochs=model.epochs)
+    if cbow is True:
+        skip = 0
+    else:
+        skip = 1
+    model = FastText(size=size, window=window,
+                     min_count=min_count, workers=workers, sg=skip,
+                     negative=negative, seed=seed)
+    model.build_vocab(sentences)
+    model.train(sentences, total_examples=model.corpus_count, epochs=iterations)
     return model
